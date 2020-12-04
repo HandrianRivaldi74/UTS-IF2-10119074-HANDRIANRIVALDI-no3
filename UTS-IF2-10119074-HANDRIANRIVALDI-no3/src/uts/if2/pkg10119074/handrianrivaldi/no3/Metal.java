6bb74cd4982b01f9623808d4ad8994b794bc87e4
HandrianRivaldi74 <72905859+HandrianRivaldi74@users.noreply.github.com>
2020-12-04 22:14:53 +0700

    Add files via upload

---
+++ UTS-IF2-10119074-HANDRIANRIVALDI-no3/src/uts/if2/pkg10119074/handrianrivaldi/no3/Metal.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.if2.pkg10119074.handrianrivaldi.no3;
+
+/**
+ *
+ * @author Acer
+ */
+public class Metal {
+    
+}
---
+++ UTS-IF2-10119074-HANDRIANRIVALDI-no3/src/uts/if2/pkg10119074/handrianrivaldi/no3/UTSIF210119074HANDRIANRIVALDINo3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.if2.pkg10119074.handrianrivaldi.no3;
+
+/**
+ *
+ * @author Acer
+ * NAMA : HANDRIAN RIVALDI
+ * KELAS : PBOIF2
+ * NIM : 10119074
+ *
+ */
+public class UTSIF210119074HANDRIANRIVALDINo3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
